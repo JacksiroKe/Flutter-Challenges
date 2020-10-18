ae1b109591493dace0819981886354786acba2f8
JacksiroKe <Jacksiroke@gmail.com>
2020-10-18 03:41:14 +0300

    android migration

--- android/app/src/main/java/com/example/playgroundflutter/MainActivity.java
+++ android/app/src/main/java/com/example/playgroundflutter/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.playgroundflutter;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterFragmentActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterFragmentActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
